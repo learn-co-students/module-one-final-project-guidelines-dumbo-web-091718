9b3ebbee9243621bb5c638eb87a8238f637f7009
github username <github email address>
2018-10-02 15:31:23 -0400

    fixed

--- lib/pet.rb
+++ lib/pet.rb
@@ -1,8 +1,4 @@
 class Pet < ActiveRecord::Base
   has_many :adoptions
-<<<<<<< HEAD
   has_many :users, through: :adoptions
-=======
-   has_many :users, through: :adoptions
->>>>>>> 1773b701d53d8a971cd88d2181e29856018492e0
 end
