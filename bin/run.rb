5edc84a9ab8a9d0d25fd2770356009aa69319296
github username <github email address>
2018-10-02 15:09:09 -0400

    added birds and cats

--- bin/run.rb
+++ bin/run.rb
@@ -1,52 +1,61 @@
 require_relative '../config/environment'
-
-# _________________________________________________
-# def welcome_and_get_name
-#   puts "Welcome to the Fuzzy Friends Pet Adoption Center! We just have a few questions for you to match you with your perfect fuzzy friend. What is your name?"
-#   name = gets.chomp
+#
+# # _________________________________________________
+# # def welcome_and_get_name
+# #   puts "Welcome to the Fuzzy Friends Pet Adoption Center! We just have a few questions for you to match you with your perfect fuzzy friend. What is your name?"
+# #   name = gets.chomp
+# # end
+# #
+# # def get_wanted_pet(name)
+# #   puts "Hey, #{name}! What kind of pet do you want to adopt?"
+# # end
+# #
+# # def run
+# #   name = welcome_and_get_name
+# #   get_wanted_pet(name)
+# # end
+# #
+# # run
+# # __________________________________________________
+#
+# # def application
+#     def create_user
+#     puts "Welcome to the Fuzzy Friends Pet Adoption Center! We just have a few questions for you to match you with your perfect fuzzy friend. What is your full name?"
+#       name = gets.chomp
+#       User.create(name: name)
+#     end
+#
+#     def choose_pet
+#     puts "What kind of pet are you applying for?"
+#       pet_type = gets.chomp
+#
+#     puts "What age?"
+#       age = gets.chomp
+#
+#     puts "What kind of pet are you looking for? (calm, excitable, shy, cute)"
+#       temperament = gets.chomp
+#     end
+#     # puts "Based on your application we think #{pet_name} would be perfect for you! Here's a little more information about them!"
+#     #list of pet info
+#     # puts "Do you want to give #{pet_name} a forever home?"
+#     #   adopt = gets.chomp
+#
+#   #   adopted = Pet.create({ name:name,
+#   #   #pet_routine: pet_routine,
+#   #   temperament: temperament,
+#   # #  color: color,
+#   #   skills: skills, age: age})
+#   puts "Congratulations! Based on your application we think bob would be perfect for you. Bob is a #{pet_type} and has a #{temperament} temperament. Bob is #{age} years old."
+#
+# # end
+#
+# # adopted = application
+#
+#  # puts "Congratulations! Based on your application we think bob would be perfect for you, they are a #{pet_type} have a #{temperament} temperament and is #{age} years old."
+#
+# def run_app
+#   user = create_user
+#   choose_pet
 # end
 #
-# def get_wanted_pet(name)
-#   puts "Hey, #{name}! What kind of pet do you want to adopt?"
-# end
-#
-# def run
-#   name = welcome_and_get_name
-#   get_wanted_pet(name)
-# end
-#
-# run
-# __________________________________________________
-
-# def application
-
-    puts "Welcome to the Fuzzy Friends Pet Adoption Center! We just have a few questions for you to match you with your perfect fuzzy friend. What is your full name?"
-      name = gets.chomp
-      new_user = User.create(name: name)
-
-    puts "What kind of pet are you applying for?"
-      pet_type = gets.chomp
-
-    puts "What age?"
-      age = gets.chomp
-
-    puts "What kind of pet are you looking for? (calm, excitable, shy, cute)"
-      temperament = gets.chomp
-
-    # puts "Based on your application we think #{pet_name} would be perfect for you! Here's a little more information about them!"
-    #list of pet info
-    # puts "Do you want to give #{pet_name} a forever home?"
-    #   adopt = gets.chomp
-
-  #   adopted = Pet.create({ name:name,
-  #   #pet_routine: pet_routine,
-  #   temperament: temperament,
-  # #  color: color,
-  #   skills: skills, age: age})
-  puts "Congratulations! Based on your application we think bob would be perfect for you. Bob is a #{pet_type} and has a #{temperament} temperament. Bob is #{age} years old."
-
-# end
-
-# adopted = application
-
- # puts "Congratulations! Based on your application we think bob would be perfect for you, they are a #{pet_type} have a #{temperament} temperament and is #{age} years old."
+# run_app
---
+++ db/seeds.rb
@@ -0,0 +1,32 @@
+# t.string :name
+# t.string :kind
+# t.string :temperament
+# t.integer :age
+
+
+# birds
+coocoo = Pet.create({name: "Coocoo", kind: "bird", temperament: "friendly", age: 22})
+charlie = Pet.create({name: "Charlie", kind: "bird", temperament: "friendly", age: 4})
+moe = Pet.create({name: "Moe", kind: "bird", temperament: "friendly", age: 10})
+sinatra = Pet.create({name: "Sinatra", kind: "bird", temperament: "calm", age: 30})
+frankie = Pet.create({name: "Frankie", kind: "bird", temperament: "calm", age: 45})
+tweetie = Pet.create({name: "Tweetie", kind: "bird", temperament: "calm", age: 100})
+kiwi = Pet.create({name: "Kiwi", kind: "bird", temperament: "shy", age: 2})
+flyboy = Pet.create({name: "Flyboy", kind: "bird", temperament: "shy", age: 3})
+whooshie = Pet.create({name: "Whooshie", kind: "bird", temperament: "shy", age: 60})
+paco = Pet.create({name: "Paco", kind: "bird", temperament: "shy", age: 6})
+
+# cats
+
+garfield = Pet.create({name: "Garfield", kind: "cat", temperament: "friendly", age: 3})
+meowie = Pet.create({name: "Meowie", kind: "cat", temperament: "friendly", age: 11})
+nala = Pet.create({name: "Nala", kind: "cat", temperament: "friendly", age: 2})
+scooter = Pet.create({name: "Scooter", kind: "cat", temperament: "calm", age: 1})
+tiger = Pet.create({name: "Tiger", kind: "cat", temperament: "calm", age: 4})
+bosco = Pet.create({name: "Bosco", kind: "cat", temperament: "calm", age: 7})
+meowyonce = Pet.create({name: "Meowyonce", kind: "cat", temperament: "calm", age: 6})
+scoop = Pet.create({name: "Scoop", kind: "cat", temperament: "shy", age: 5})
+stormy = Pet.create({name: "Stormy", kind: "cat", temperament: "shy", age: 13})
+miss_kitty = Pet.create({name: "Miss Kitty", kind: "cat", temperament: "shy", age: 8})
+
+# dogs
