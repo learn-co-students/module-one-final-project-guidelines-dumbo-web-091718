13c90a80254a8d108e255953a449677f066dac10
shemarjoseph <shemarjoseph@yahoo.com>
2018-10-04 15:25:50 -0400

    added the ability to navigate walls

--- lib/helpers/wallGuide.rb
+++ lib/helpers/wallGuide.rb
@@ -1,5 +1,9 @@
 class WallGuide
 
+  def self.prompt
+    TTY::Prompt.new
+  end
+
   def self.display_walls
     puts "All Walls:"
     Wall.all.each do |wall|
@@ -12,19 +16,43 @@ class WallGuide
   end
 
   def self.choose_wall
-    WallGuide.display_walls
-    puts "Which Wall# do you want to visit?"
-    Escort.current_wall_num = gets.chomp
-    while true
-      if WallGuide.has_permission?(Escort.current_user, Escort.current_wall_num)
-        Escort.options
-      else
-        puts "403: You don't have permission to access that."
-        puts "Choose another wall"
-        Escort.current_wall_num = gets.chomp
-      end
+    # prompt.select("Which wall do you want to visit?") do |menu|
+    #   menu.default 3
+    
+    #   menu.choice 'Scorpion', 1
+    #   menu.choice 'Kano', 2
+    #   menu.choice 'Jax', 3
+    # end
+    walls = Wall.all.map { |wall| wall }
+    wall_names = walls.map { |wall| wall.name }
+    wall_name = prompt.select('Which wall do you want to visit?', wall_names)
+    # WallGuide.display_walls
+    # puts "Which Wall # do you want to visit?"
+    found_wall = Wall.find_by(name: wall_name).id
+    Escort.current_wall_num = found_wall 
+    # binding.pry
+    if WallGuide.has_permission?(Escort.current_user, Escort.current_wall_num)
+      Escort.options
+    else
+      self.choose_wall 
+      # puts "403: You don't have permission to access that."
+      # puts "Choose another wall"
+      # Escort.current_wall_num = gets.chomp
     end
   end
+
+  # def self.choose_wall
+  #   # Present a list of walls to the user
+  #   # If the user selects a wall that he doesn't have permission to view, he's presented with a 
+  #   # list of walls again 
+  #   walls = Wall.map { |wall| wall } 
+  #   wall_names = walls.map { |wall| wall.name }
+  #   wall_name = prompt.select('Which wall do you want to visit?', wall_names)
+  #   found_wall = Wall.find_by(name: wall_name).id 
+  #   Escort.current_wall_num = found_wall 
+
+  #   if WallGuide.has_permission??(Escort.current_user, Escort.c)
+  # end
 
 
   def self.read
@@ -60,10 +88,11 @@ class WallGuide
 
   def self.my_posts
     Message.where(:user_id => Escort.current_user.id).each do |msg|
+      binding.pry
       puts "Wall: #{Wall.find_by(:id => msg.wall_id).name}"
       puts "Date: #{msg.created_at}"
       puts "#{msg.content}"
-    Escort.options
     end
+    Escort.options
   end
 end
