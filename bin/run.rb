781c0c5f4194092bdcb18ddfe4e66144a33a0c4b
github username <github email address>
2018-10-04 15:07:32 -0400

    added to getting user pet

--- bin/run.rb
+++ bin/run.rb
@@ -1,4 +1,5 @@
 require_relative '../config/environment'
+require 'colorize'
 
    def welcome_screen
   puts "Hey, welcome to Fuzzy Friends. Just looking or wanting to adopt?"
@@ -23,9 +24,10 @@ require_relative '../config/environment'
   end
 
   def print_pets
-    all_the_pets = Pet.all.each do | pet |
-      puts "#{pet.name}: #{pet.kind}, #{pet.temperament}, #{pet.age}"
-    end
+    # all_the_pets = Pet.all.each do | pet |
+    #   puts "#{pet.name}: #{pet.kind}, #{pet.temperament}, #{pet.age}"
+    puts "okie dokie"
+
   end
 
        ##### USER APPLICATION #####
@@ -45,11 +47,11 @@ def create_user
 
   def approve_user(new_user)  # Approve User
     # min = 2
-      if new_user.income >= 200000000
-        puts "Congrats!! your application is APPROVED!!! Yay!!"
+      if new_user.income >= 200
+        puts "Congrats! your application is APPROVED!!! Yay!!".colorize(:light_blue)
         true
       else
-        puts "Sorry. Application DENIED!"
+        puts "Sorry but your application has been denied.".colorize(:red)
         false
       end
 
@@ -69,33 +71,70 @@ end
      ##### PET APPLICATION! #####
   # get the pet based on user preference
 def get_users_pet
-  puts "What kind of pet would you want"
-  user_pet_kind = gets.chomp
+  puts "What kind of pet would you want?"
+  user_pet_kind = gets.chomp.downcase
   puts "What kind of temperament would you want your pet to have?"
-  user_pet_temper = gets.chomp
-    the_pet = Pet.all.find do |pet|
-          pet.kind == user_pet_kind && pet.temperament == user_pet_temper
-          end
-  # puts "Here is your pet, it's name is #{the_pet.name}"
-  puts "What's the ideal age of your pet?"
-  # enter number (ex. 4)
-  #if/else statement number > 5 return that pet that matches
-  #elsif return pet that is > 10 return that pet that matches
-  #etc.
-  puts "Based on your preferences we think #{pet.name} would be perfect for you! Would you like to meet you pet?"
-  #puts "1. 'Gosh, so excited. Yes!'"
-  #puts "2. 'Meh, I guess."
-  #puts "3. 'No, pets are gross.'"
-  # if 1 || 2 => shows picture of pet
-  # if 3 => puts "Uh. ok. See ya later!""
-  puts "#{pet.name} really seems to like you! Do you want to adopt them?"
-  #puts "1. 'YES!'"
-  #puts "2. 'They're kind of ugly. No.'"
-  #if 1 puts "Yay!"
-  #puts something exciting like a congrats thing
-  #if 2 puts "Sad... ok. well bye."
-  #exits program
+  user_pet_temper = gets.chomp.downcase
+  puts "what is the preferred age of your pet?"
+  user_pet_age = gets.chomp.to_i
+
+  pet_ages = Pet.all.map do |pet|
+    pet.age
+  end
+
+  closest_age = pet_ages.min_by { |x| (user_pet_age - x).abs }
+
+  
+  the_pet = Pet.find_by(age: closest_age, temperament: user_pet_temper, kind: user_pet_kind)
+  # binding.pry
+
+  # def sort_age
+  #   pet_guy = Pet.all.find do |pet|
+  #     pet.age >= user_pet_age
+  #   end
+  # end
+
+  # pet_ages = Pet.all.map do |pet|
+  #   pet.age
+  # end
+  #
+  # binding.pry
+  #
+  # closest_age = pet_ages.min_by { |x| (user_pet_age - x).abs }
+  #
+  #
+  #
+  #   the_pet = Pet.all.find do |pet|
+  #         # pet.kind == user_pet_kind && pet.temperament == user_pet_temper && sort_age
+  #         pet.age == closest_age
+  #
+  #         end
+  # # puts "Here is your pet, it's name is #{the_pet.name}"
+  # # puts "What's the ideal age of your pet?"
+  # # enter number (ex. 4)
+  #
+  #
+  # #if/else statement number > 5 return the pet that matches
+  # #elsif return pet that is > 10 return the pet that matches
+  # #etc.
+  # binding.pry
+  puts "Based on your preferences we think #{the_pet.name}."
+  #
+  # #puts "1. 'Gosh, so excited. Yes!'"
+  # #puts "2. 'Meh, I guess."
+  # #puts "3. 'No, pets are gross.'"
+  # # if 1 || 2 => shows picture of pet
+  # # if 3 => puts "Uh. ok. See ya later!""
+  # puts "#{the_pet.name} really seems to like you! Do you want to adopt them?"
+  # #puts "1. 'YES!'"
+  # #puts "2. 'They're kind of ugly. No.'"
+  # #if 1 puts "Yay!"
+  # #puts something exciting like a congrats thing
+  # #if 2 puts "Sad... ok. well bye."
+  # #exits program
 end
+
+
 def run_app
   welcome_response = welcome_screen
   if welcome_response != "1"
@@ -114,8 +153,12 @@ def run_app
       return
     end
   end
-  puts "I'm still here!!!!"
+
+  get_users_pet
 
 end
 
 run_app
+
+#can adopt multiple animals
+#can view all their animals
