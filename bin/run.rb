775a9a9a5043b7b324cb9012b16faf8405dc0dd7
github username <github email address>
2018-10-04 20:02:31 -0400

    trying new things

--- bin/run.rb
+++ bin/run.rb
@@ -1,26 +1,43 @@
 require_relative '../config/environment'
 require 'colorize'
 
+pid = fork{ exec 'afplay',"Nyanyanyanyanyanyanya.mp3"}
    def welcome_screen
-  puts "Hey, welcome to Fuzzy Friends. Just looking or wanting to adopt?"
-  puts "1. Just looking!"
+    puts "Hey, welcome to Fuzzy Friends. Just looking or wanting to adopt?"
+    puts "1. Just looking!"
 
-  puts "2. I wanna pet!"
-      user_choice = gets.chomp
+    puts "2. I wanna pet!"
 
+
+        user_choice = gets.chomp
   end
+
+
   def second_chance
-    puts "====================="
-    puts "Pretty good selection, huh? Now do you want a pet?!"
-    puts "1. Yes!"
-    puts "2. No."
+    puts "Are you SURE you don't want a pet?"
+    puts "1. Okkkkk, I'll get one."
+    puts "2. No. I hate cute things."
     user_second_chance = gets.chomp
   end
+
 
   def print_pets
     # all_the_pets = Pet.all.each do | pet |
     #   puts "#{pet.name}: #{pet.kind}, #{pet.temperament}, #{pet.age}"
-    puts "okie dokie"
+    puts <<-'EOF'
+                 ________________
+                |                |_____    __
+                |  I Love You!   |     |__|  |_________
+                |________________|     |::|  |        /
+   /\**/\       |                \.____|::|__|      <
+  ( o_o  )_     |                      \::/  \._______\
+   (u--u   \_)  |
+    (||___   )==\
+  ,dP"/b/=( /P"/b\
+  |8 || 8\=== || 8
+  `b,  ,P  `b,  ,P
+    """`     """
+    EOF
 
   end
 
@@ -29,41 +46,55 @@ def create_user
   puts "Great! Before we find you the perfect pet. We need you to fill out a quick application."
   puts "====================="
   puts "What's your name?"  #1
-  name = gets.chomp
+    name = gets.chomp
   puts "What city do you live in?" #2
-  city = gets.chomp
+    city = gets.chomp
   puts "Last 4 digits of your Social Security?"
-  ssn = gets.chomp.to_i
-  income  = rand(ssn)
+    ssn = gets.chomp.to_i
+    income  = rand(ssn)
   #randomize income range "based on SS"
-  puts "We did a Background-Check and your income is: $#{income}/month."
-  new_user = User.create({name: name, city: city, income:income }) #Create User
+  puts "We did a Background-Check and your income is $#{income}/month."
+    new_user = User.create({name: name, city: city, income:income }) #Create User
+    approve_user(new_user)
+end
 
-  def approve_user(new_user)  # Approve User
-    # min = 2
-      if new_user.income >= 100
-        puts "Congrats! Your application is APPROVED!!! Yay!!".colorize(:green)
-        new_user
-      else
-        puts "Yikes... your application has been denied.".colorize(:red)
-        false
-      end
+def approve_user(new_user)
+    if new_user.income >= 100
+      puts "Congrats! Your application is APPROVED! Yay! Let's find you a pet. ✔ ".colorize(:green)
+      new_user
+    else
+      puts "Yikes... your application has been denied.".colorize(:red)
+      false
+    end
 
-  end
-
+end
 
 def get_users_pet(user)
+  puts "❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤"
+  puts "    ❤      ❤     ❤     ❤      ❤      ❤      ❤       "
   puts "What kind of pet would you want? (Cat, Dog, or Bird)"
+  puts "    ❤      ❤     ❤     ❤      ❤      ❤      ❤       "
+  puts "❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤"
+
     user_pet_kind = gets.chomp.downcase
+  puts "❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤"
+  puts "    ❤      ❤     ❤     ❤      ❤      ❤      ❤       "
   puts "What kind of temperament would you want your pet to have? (Calm, Shy, or Friendly)"
+  puts "    ❤      ❤     ❤     ❤      ❤      ❤      ❤       "
+  puts "❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤"
     user_pet_temper = gets.chomp.downcase
+  puts "❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤"
+  puts "    ❤      ❤     ❤     ❤      ❤      ❤      ❤       "
   puts "what is the preferred age of your pet?"
+  puts "    ❤      ❤     ❤     ❤      ❤      ❤      ❤       "
+  puts "❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤"
+
     user_pet_age = gets.chomp.to_i
 
 
   the_pet = Pet.all.find do |pet|
-              pet.kind == user_pet_kind && pet.temperament == user_pet_temper && pet.age == user_pet_age
-            end
+    pet.kind == user_pet_kind && pet.temperament == user_pet_temper && pet.age == user_pet_age
+  end
 
 
   if the_pet
@@ -80,11 +111,11 @@ def get_users_pet(user)
       puts "We have #{pet.name} who's #{pet.age}-years-old."
     end
     puts "Which pet do ya like?"
-    user_select_name = gets.chomp
-    puts "You like #{user_select_name}! Great #{user.name}! I'm sure you'll have a happy life together."
+    user_select_name = gets.chomp.capitalize
+    puts "You like #{user_select_name}! Great #{user.name.capitalize}! I'm sure you'll have a happy life together."
     the_final_pet = the_pets.find do |pet|
       pet.name == user_select_name
-      end
+    end
     Adoption.create(user_id: user.id, pet_id: the_final_pet.id)
   end
 end
@@ -101,17 +132,15 @@ def run_app
       created_user = create_user
       return if !created_user
     else
-      puts "Ok... cool. Have a nice day!"
+      puts "Ok... loser. Never show your face here again!"
       return
     end
-
 
   end
 
   get_users_pet(created_user)
 
-
-
 end
 
 run_app
+exec("kill #{pid}")
