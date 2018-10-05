81348418bdb321b9d4f67a8f08ffe8d2ec4c0106
github username <github email address>
2018-10-05 12:45:08 -0400

    added judge sounds

--- bin/run.rb
+++ bin/run.rb
@@ -277,46 +277,30 @@ def judge_scores(band)
   Judge.all.each do |judge|
     judge_scores[judge.name] = judge.grade(band)
   end
-  # judge_responses = self.map do |judge|
-  #   judge.content
-  # end
-  # binding.pry
 
 
   return judge_scores
 end
 
-# def evaluate_scores
-#   responses = Judge.all.each do |judge|
-#     judge.judge_responses
-#   end
-#   binding.pry
-#   responses
-# end
 
 def audience_scores(band)
   @audience = band.create_audience
   return @audience.grade(band)
 end
 #
-# # run method right before play_band to make sure all columns have their proper input
-#
-# def judge_responses(name)
-#   sel_judge = Judge.find_by(name: name)
-#   responses = sel_judge.responses.map do |response|
-#     response.content
-#   end
-#   responses
-#   # binding.pry
-#   # Response.all.select do |response|
-#   #   response.judge_id == sel_judge.id
-#   # end.content
-# end
 #
 
 def return_responses(band)
-  responses = Judge.all.each {|judge| judge.evaluate_scores(band)}
-
+  clear_screen
+  puts "And now, let's hear from the judges!"
+  sleep(3)
+  Judge.all.each do |judge|
+    puts ""
+    puts "#{judge.name}'s Response"
+    puts ""
+    judge.evaluate_scores(band)
+    sleep(5)
+  end
 end
 
 
@@ -327,7 +311,7 @@ def play_band
     create_user_2
     return_responses(@band_1)
     # binding.pry
-    # return_responses(@band_2)
+    return_responses(@band_2)
 
 
 #   #binding.pry
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,18 +17,24 @@ simon = Judge.create(name: "Simon Cowell", preferred_att1: "technical ability", 
 paula = Judge.create(name: "Paula Abdul", preferred_att1: "stage_presence", preferred_att2: ["tech_ability", "presentation", "lyrics"].sample)
 randy = Judge.create(name: "Randy Jackson", preferred_att1: "lyrics", preferred_att2: "presentation")
 
+
 #Simon Response
 bad = Response.create(judge: simon, content: "Absolutely Dreadful", score: 0)
 ok = Response.create(judge: simon, content: "Although it was good", score: 1)
 good = Response.create(judge: simon, content: "Im pleased", score: 2)
 
 #Paula Response
-bad = Response.create(judge: paula, content: "Awful", score: 0)
-ok = Response.create(judge: paula, content: "Ultra cool", score: 1)
+bad = Response.create(judge: paula, content: "Oh boy", score: 0)
+ok = Response.create(judge: paula, content: "I dont know", score: 1)
 good = Response.create(judge: paula, content: "You gave us chills", score: 2)
 #
 
 #Randy Response
-bad = Response.create(judge: randy, content: "Bad", score: 0)
-ok = Response.create(judge: randy, content: "Eh", score: 1)
-good = Response.create(judge: randy, content: "Awesome", score: 2)
+bad = Response.create(judge: randy, content: "It was a little weird", score: 0)
+ok = Response.create(judge: randy, content: "It was alright", score: 1)
+good = Response.create(judge: randy, content: "It was kinda hot", score: 2)
+
+#Audience response
+bad = Response.create(judge: randy, content: "Awful", score: 0)
+ok = Response.create(judge: randy, content: "Do Better", score: 1)
+good = Response.create(judge: randy, content: "Amazing", score: 2)
